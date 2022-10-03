198f3c38324c19fcd607955bd95d9e2c5c05a410
Your Name <moisesjuniorcas20@gmail.com>
2022-10-03 11:27:05 -0500

    subiendo contenido

---
+++ src/pages/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Add from "../img/addAvatar.png";
+
+
+const Register = () => {
+  return (
+    <div className='formContainer' >
+        <div className='formWrapper'>
+        <span className='logo'>Chat Soporte Ciencias Humanas</span>
+        <span className='title'>Registro</span>
+
+            <form>
+                <input type='text' placeholder='Username' />
+                <input type='email' placeholder='Email' />
+                <input type='password' placeholder='Password' />
+                <input style={{display:"none"}} type='file' placeholder='photo' id='file' /> 
+                <label htmlFor='file'> 
+                    <img src={Add} alt='' />
+                    <span>Subir foto</span>
+                </label>
+                <button type='submit'>Register</button>
+            </form>
+            <p>¿Tiene cuenta? Realiza login</p> 
+        </div>
+
+    </div>
+  )
+}
+
+export default Register 
