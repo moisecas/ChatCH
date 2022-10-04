62e0f3bbd567dfdb3151165e7eccc7f28602fa08
Your Name <moisesjuniorcas20@gmail.com>
2022-10-04 16:05:15 -0500

    interfaces del chat

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -7,6 +7,21 @@ const Chats = () => {
         <img src='https://images.pexels.com/photos/8717975/pexels-photo-8717975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
         <div className='userChatInfo'>
           <span>Nombre</span>
+          <p>Hola</p>
+        </div>
+      </div>
+      <div className='userChat'>
+        <img src='https://images.pexels.com/photos/8717975/pexels-photo-8717975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+        <div className='userChatInfo'>
+          <span>Nombre2</span>
+          <p>Hola</p>
+        </div>
+      </div>
+      <div className='userChat'>
+        <img src='https://images.pexels.com/photos/8717975/pexels-photo-8717975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+        <div className='userChatInfo'>
+          <span>Nombre3</span>
+          <p>Hola</p>
         </div>
       </div>
     </div>
