7c87da2a7f80a2b8a462f13ad02f55290cdd2765
Your Name <moisesjuniorcas20@gmail.com>
2022-10-05 10:24:07 -0500

    agregando css chats

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message' >
-    Message
+    <div className='message owner' >
+      <div className='messageInfo'>
+        <img src='https://images.pexels.com/photos/13682725/pexels-photo-13682725.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+        <span className=''>Ahora</span>
+      </div>
+      <div className='messageContent'>
+        <p>Este es un mensaje</p> 
+        <img src='https://images.pexels.com/photos/13682725/pexels-photo-13682725.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+
+      </div>
     </div>
   )
 }
